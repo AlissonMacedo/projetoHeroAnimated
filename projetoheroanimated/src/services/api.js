a37151d8d71b9651291fe4b932d0a085611f17b5
Alisson Macedo <alissontecnico@gmail.com>
2019-12-08 20:04:32 -0300

    add

--- projetoheroanimated/src/services/api.js
+++ projetoheroanimated/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://api.github.com',
+  baseURL: 'https://superheroapi.com/api/1467143070109568/',
 });
 
 export default api;
