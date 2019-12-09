6c00ba826deb14a6798911bbed3f611e65b0e6ae
Alisson Macedo <alissontecnico@gmail.com>
2019-12-08 21:18:06 -0300

    add styles in home

--- projetoheroanimated/src/pages/Main/index.js
+++ projetoheroanimated/src/pages/Main/index.js
@@ -1,16 +1,46 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import {
+  View,
+  Text,
+  Image,
+  TouchableOpacity,
+  SafeAreaView,
+} from 'react-native';
 
-import { Container } from './styles';
+import { Container, CardMain, ListCards, HeaderSearch } from './styles';
+
+import Batman from '../../assets/batman.png';
 
 export default function Main() {
   return (
     <>
-      <Container>
-        <View>
-          <Text>Teste</Text>
-        </View>
-      </Container>
+      <SafeAreaView style={{ flex: 1 }}>
+        <HeaderSearch>
+          <Text>Alisson</Text>
+          <Text>Alisson</Text>
+          <Text>Alisson</Text>
+        </HeaderSearch>
+        <Container>
+          <CardMain>
+            <Image source={Batman} style={{ height: 200, width: 200 }} />
+            <TouchableOpacity
+              style={{
+                justifyContent: 'center',
+                alignItems: 'center',
+                backgroundColor: '#7159c1',
+                borderRadius: 15,
+                height: 30,
+                width: 120,
+                marginTop: 40,
+              }}>
+              <Text style={{ fontWeight: 'bold', fontSize: 15, color: '#FFF' }}>
+                EXPLORE
+              </Text>
+            </TouchableOpacity>
+          </CardMain>
+          <ListCards />
+        </Container>
+      </SafeAreaView>
     </>
   );
 }
--- projetoheroanimated/src/pages/Main/styles.js
+++ projetoheroanimated/src/pages/Main/styles.js
@@ -2,7 +2,37 @@ import styled from 'styled-components/native';
 
 export const Container = styled.View`
   flex: 1;
-  justify-content: center;
+  flex-direction: column;
+  justify-content: flex-start;
   align-items: center;
   background-color: #999;
+`;
+
+export const HeaderSearch = styled.View`
+  flex-direction: row;
+  align-items: center;
+  margin-left: 10;
+  margin-right: 10;
+  justify-content: space-around;
+  background-color: #fff;
+  height: 40;
+  width: 100%;
+`;
+
+export const CardMain = styled.View`
+  justify-content: center;
+  align-items: center;
+  height: 60%;
+  width: 100%;
+  background-color: #000;
+  border-bottom-left-radius: 60;
+  border-bottom-right-radius: 60;
+  position: absolute;
+  z-index: 1;
+`;
+
+export const ListCards = styled.View`
+  background-color: #fff;
+  width: 100%;
+  flex: 1;
 `;
